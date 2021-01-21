a576e7da48df96c5afe3430a924cafe4914762aa
Eylam Tagor <eylamtagor@gmail.com>
2021-01-21 11:22:23 -0800

    Added HomeActivity w/ default fragments, renamed Main to LoginActivity

---
+++ app/src/main/java/com/hhsfbla/hhs_fbla_mad_2021/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.hhsfbla.hhs_fbla_mad_2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button testButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        testButton = findViewById(R.id.testButton);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            }
+        });
+    }
+}
