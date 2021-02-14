1a7502dc7e98bdbc9c5bb0caddf7fb7a63fe109f
Sky Ng-Thow-Hing <skyngthowhing@gmail.com>
2021-02-13 21:31:42 -0800

    Added other profile page activity for when other ppl look at your profile

---
+++ app/src/main/java/com/hhsfbla/hhs_fbla_mad_2021/activities/OtherProfileActivity.java
@@ -0,0 +1,16 @@
+package com.hhsfbla.hhs_fbla_mad_2021.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hhsfbla.hhs_fbla_mad_2021.R;
+
+// When someone else views your profile
+public class OtherProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_profile);
+    }
+}
