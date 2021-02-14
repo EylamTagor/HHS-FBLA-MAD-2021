a960b0b66a40a6b6896c27ed07319c2cd4ff3abb
Sky Ng-Thow-Hing <skyngthowhing@gmail.com>
2021-02-13 21:24:00 -0800

    created businesspage xml and edited profile page, added following and liked posts

---
+++ app/src/main/java/com/hhsfbla/hhs_fbla_mad_2021/activities/MyBusinessActivity.java
@@ -0,0 +1,19 @@
+package com.hhsfbla.hhs_fbla_mad_2021.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.hhsfbla.hhs_fbla_mad_2021.R;
+
+public class MyBusinessActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_business);
+    }
+}
