7db25974188a9590eddec8300a7a38d32c196dc4
Sky Ng-Thow-Hing <skyngthowhing@gmail.com>
2021-02-17 22:46:44 -0800

    Jobs page UI done

---
+++ app/src/main/java/com/hhsfbla/hhs_fbla_mad_2021/JobsRVModel.java
@@ -0,0 +1,30 @@
+package com.hhsfbla.hhs_fbla_mad_2021;
+
+import com.hhsfbla.hhs_fbla_mad_2021.classes.Business;
+import com.hhsfbla.hhs_fbla_mad_2021.classes.Experience;
+import com.hhsfbla.hhs_fbla_mad_2021.classes.JobOffer;
+
+public class JobsRVModel {
+
+    private JobOffer job;
+    public void addJob(JobOffer jobOffer) {
+        this.job = job;
+    }
+
+    public String getJobTitle() {
+        return job.getJobTitle();
+    }
+    public String getbusinessID() {
+        return job.getBusinessID();
+    }
+    public String getLink() {
+        return job.getLink();
+    }
+    public String getJobDescription() { return job.getJobDescription(); }
+
+
+
+    public JobsRVModel(JobOffer job) {
+        this.job = job;
+    }
+}
