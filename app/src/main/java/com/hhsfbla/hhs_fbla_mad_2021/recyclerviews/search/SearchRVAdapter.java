155b3a69af413204a3ed9a9e4ee95543d2960847
Sky Ng-Thow-Hing <skyngthowhing@gmail.com>
2021-02-25 19:50:45 -0800

    javadocs

--- app/src/main/java/com/hhsfbla/hhs_fbla_mad_2021/recyclerviews/search/SearchRVAdapter.java
+++ app/src/main/java/com/hhsfbla/hhs_fbla_mad_2021/recyclerviews/search/SearchRVAdapter.java
@@ -42,6 +42,14 @@ public class SearchRVAdapter extends RecyclerView.Adapter<SearchRVAdapter.RVView
         db = FirebaseFirestore.getInstance();
     }
 
+    /**
+     *
+     * The ViewHolder will be used to display items of the adapter using onBindViewHolder.
+     *
+     * @param parent The ViewGroup into which the new View will be added after it is bound to an adapter position.
+     * @param viewType  The view type of the new View.
+     * @return the view holder to be used
+     */
     @NonNull
     @Override
     public RVViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -50,16 +58,21 @@ public class SearchRVAdapter extends RecyclerView.Adapter<SearchRVAdapter.RVView
         return rvViewHolder;
     }
 
+    /**
+     *Called by RecyclerView to display the data at the specified position.
+     *This method should update the contents of the RecyclerView.ViewHolder.itemView to reflect the item at the given position.
+     *
+     * @param holder The ViewHolder which should be updated to represent the contents of the item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(@NonNull RVViewHolder holder, int position) {
         SearchRVModel currentItem = searches.get(position);
         holder.name.setText(currentItem.getName());
         holder.header.setText(currentItem.getHeader());
-
         db = FirebaseFirestore.getInstance();
         fbuser = FirebaseAuth.getInstance().getCurrentUser();
 
-        //BACKEND GET PFP THIS IS A PLACEHOLDER
         db.collection("users").document(currentItem.getId()).get().addOnSuccessListener(documentSnapshot -> {
             User u = documentSnapshot.toObject(User.class);
 
@@ -100,11 +113,16 @@ public class SearchRVAdapter extends RecyclerView.Adapter<SearchRVAdapter.RVView
         searchesFull = new ArrayList<>(searches);
     }
 
+    /**
+     *
+     * @return returns the filter
+     */
     @Override
     public Filter getFilter() {
         return searchesFilter;
     }
 
+    //Creates the search filter
     private Filter searchesFilter = new Filter() {
 
         @Override
@@ -127,6 +145,11 @@ public class SearchRVAdapter extends RecyclerView.Adapter<SearchRVAdapter.RVView
             return results;
         }
 
+        /**
+         * publishes the filter results, updates the list of the search results.
+         * @param charSequence
+         * @param filterResults the results of the filter.
+         */
         @Override
         protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
             searches.clear();
@@ -135,6 +158,9 @@ public class SearchRVAdapter extends RecyclerView.Adapter<SearchRVAdapter.RVView
         }
     };
 
+    /**
+     * The ViewHolder will be used to display items. Links the holder to the XML file
+     */
     public class RVViewHolder extends RecyclerView.ViewHolder {
         private TextView name;
         private TextView header;
