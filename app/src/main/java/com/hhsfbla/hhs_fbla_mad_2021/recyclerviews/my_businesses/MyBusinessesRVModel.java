e49ac31bcc43c1f732004177950459ab7710c764
AyushSat <themessimess@gmail.com>
2021-02-25 20:11:46 -0800

    javadoced all of the models

--- app/src/main/java/com/hhsfbla/hhs_fbla_mad_2021/recyclerviews/my_businesses/MyBusinessesRVModel.java
+++ app/src/main/java/com/hhsfbla/hhs_fbla_mad_2021/recyclerviews/my_businesses/MyBusinessesRVModel.java
@@ -9,19 +9,42 @@ public class MyBusinessesRVModel {
     private Business business;
     private String id;
 
+    /**
+     * Returns the logo
+     * @return the logo
+     */
     public String getLogo() {
         return business.getLogo();
     }
+
+    /**
+     * Returns the name
+     * @return the name
+     */
     public String getName() {
         return business.getName();
     }
+
+    /**
+     * Returns the website link
+     * @return the website link
+     */
     public String getWebsite() {
         return business.getWebsite();
     }
+
+    /**
+     * Returns the firebase ID
+     * @return the firebase ID
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Returns whether the business has a logo
+     * @return whether the business has a log
+     */
     public boolean hasLogo(){
         if(business.getLogo().equals("") || business.getLogo() == null ){
             return false;
@@ -31,6 +54,11 @@ public class MyBusinessesRVModel {
         }
     }
 
+    /**
+     *
+     * @param business the business to construct the model around
+     * @param id the firebase id
+     */
     public MyBusinessesRVModel(Business business, String id) {
         this.business = business;
         this.id = id;
--- app/src/main/java/com/hhsfbla/hhs_fbla_mad_2021/recyclerviews/search/SearchRVModel.java
+++ app/src/main/java/com/hhsfbla/hhs_fbla_mad_2021/recyclerviews/search/SearchRVModel.java
@@ -12,39 +12,68 @@ public class SearchRVModel {
     private Business business;
     private boolean isUser;
 
-
+    /**
+     * Returns the name
+     * @return the name
+     */
     public String getName() {
         if(isUser)
             return user.getName();
         return business.getName();
     }
 
+    /**
+     * Returns the header
+     * @return the header
+     */
     public String getHeader() {
         if(isUser)
             return user.getJobTitle();
         return business.getWebsite();
     }
 
+    /**
+     * Returns the profile picture
+     * @return the profile picture
+     */
     public String getPfp() {
         if(isUser)
             return user.getPfp();
         return business.getLogo();
     }
 
+    /**
+     * Returns the firebase ID
+     * @return the firebase ID
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Returns whether the model is a user
+     * @return whether the model is a user
+     */
     public boolean isUser() {
         return isUser;
     }
 
+    /**
+     *
+     * @param user The User object
+     * @param id the user firebase ID
+     */
     public SearchRVModel(User user, String id) {
         isUser = true;
         this.user = user;
         this.id = id;
     }
 
+    /**
+     *
+     * @param business The business object
+     * @param id the business firebase ID
+     */
     public SearchRVModel(Business business, String id) {
         isUser = false;
         this.business = business;
